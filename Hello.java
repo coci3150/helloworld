e0ee628da23fa7f1d365c06fe06e4ba4443db0b5
coci315 <coci315@gmail.com>
2018-12-21 22:02:19 +0800

    add echo func

--- Hello.java
+++ Hello.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class Hello {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
+  public static void main(String[] args) {
+    // System.out.println("Hello World");
+    System.out.println("Please input sth");
+    Scanner in = new Scanner(System.in);
+    System.out.println(in.nextLine());
+  }
 
 }
